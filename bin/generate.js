9e8d00517546bb63876e93b63c2b267ab0e4dcb3
Linda Kung <lindakung@Lindas-MacBook-Pro.local>
2016-08-03 14:26:49 -0400

    remove package.json on generate

--- bin/generate.js
+++ bin/generate.js
@@ -14,6 +14,25 @@ ncp.limit = 16;
 var newProjectDir = process.cwd();
 var generatorFilesPath = path.join(__dirname, '../generated');
 
+var removePackageJSON = function removePackageJSON() {
+    return new Promise(function(resolve, reject) {
+        if (!isWin) {
+            exec('find . -type f -name "package.json" -delete', function(error, stdout, stderr) {
+                if (error) return reject(stderr);
+                else return resolve(stdout);
+            });
+        } else {
+            exec('del /s /q .\\package.json', function(error, stdout, stderr) {
+                if (error) {
+                    return reject(stderr);
+                } else {
+                    return resolve(stdout);
+                }
+            });
+        }
+    });
+};
+
 var copyFiles = function() {
     return ncp(generatorFilesPath, newProjectDir);
 };
@@ -21,6 +40,9 @@ var copyFiles = function() {
 console.log(chalk.green('Generating dfxio files for your app!'));
 
 copyFiles()
+	.then(function() {
+		return removePackageJSON();
+	})
     .then(function() {
         var codeyBits = isWin ? 'Cmd Prompt' : 'Terminal';
         console.log(chalk.green('dfxio files have been added to your app!'));
